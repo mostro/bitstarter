64d7bb36a81410e616b4366014664a8bb5b8e470
mostro <mostro@mostro-HP.(none)>
2013-07-09 10:39:54 -0500

    Display Index

--- web.js
+++ web.js
@@ -3,10 +3,22 @@
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var HelloText = fs.readFileSync(index.html);
+  
+  response.send(HelloText.toString);
+        
+
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
+
